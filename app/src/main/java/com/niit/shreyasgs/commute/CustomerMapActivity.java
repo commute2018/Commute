dac53b722dd41d1446be05d393c6ae7c7dbf7e67
ShreyasGS <commute.application@gmail.com>
2018-09-09 19:59:40 +0530

    Integration with placesAPI with onClick

--- app/src/main/java/com/niit/shreyasgs/commute/CustomerMapActivity.java
+++ app/src/main/java/com/niit/shreyasgs/commute/CustomerMapActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
@@ -14,14 +16,24 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
@@ -35,11 +47,18 @@ import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -48,10 +67,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class CustomerMapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class CustomerMapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener {
 
     //-------PERMISSIONS-----------
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -67,24 +89,39 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
     //--------MAP DECLARATION-----
     private GoogleMap mMap;
     private FusedLocationProviderClient fusedLocationProviderClient;
+    private GeoDataClient geoDataClient;
+    private GoogleApiClient googleApiClient;
 
     //-------LAYOUT INSTANCES------
     private Toolbar customerMapToolbar;
-    private EditText customerFromInput;
-    private EditText customerToInput;
+    private AutoCompleteTextView customerFromInput;
+    private AutoCompleteTextView customerToInput;
     private Button customerRideNowButton;
+    private ImageButton searchFrom;
+    private ImageButton searchTo;
+    private ImageView myLocation;
 
     //------PAGE VARIABLES------
     String customerFrom, customerTo, currentUser;
+    private Address fromAddress,toAddress;
     private Boolean locationPermissionGranted = false;
     private static final float DEFAULT_ZOOM = 15f;
+    private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(new LatLng(-40, -168), new LatLng(71,136));
+    private PlaceAutocompleteAdapter autocompleteAdapter;
+    private placeInfo selectedPlace;
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_map);
+        googleApiClient = new GoogleApiClient.Builder(this).addApi(Places.GEO_DATA_API).addApi(Places.PLACE_DETECTION_API).enableAutoManage(this,this).build();
+        geoDataClient = Places.getGeoDataClient(this, null);
         getLocationPermission();
-
 
         //-----FIREBASE DECLARATIONS------
         CustomerAuth = FirebaseAuth.getInstance();
@@ -93,13 +130,24 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
 
         //-----LAYOUT DECLARATIONS TO BE MADE HERE
         customerMapToolbar = (Toolbar) findViewById(R.id.customer_map_activity_toolbar);
-        customerFromInput = (EditText) findViewById(R.id.customer_map_activity_from);
-        customerToInput = (EditText) findViewById(R.id.customer_map_activity_to);
+        customerFromInput = (AutoCompleteTextView) findViewById(R.id.customer_map_activity_from);
+        customerToInput = (AutoCompleteTextView) findViewById(R.id.customer_map_activity_to);
+        searchFrom = (ImageButton)findViewById(R.id.customer_map_activity_search_from);
+        searchTo = (ImageButton)findViewById(R.id.customer_map_activity_search_to);
+        myLocation = (ImageView)findViewById(R.id.customer_map_activity_find_my_loc);
         customerRideNowButton = (Button) findViewById(R.id.customer_map_activity_ride_now_button);
+        autocompleteAdapter = new PlaceAutocompleteAdapter(this, geoDataClient, LAT_LNG_BOUNDS, null);
 
         //------toolbar changes to be made here--------
         setActionBar(customerMapToolbar);
 
+        customerFromInput.setOnItemClickListener(suggestionClickListener);
+        customerToInput.setOnItemClickListener(suggestionClickListener);
+
+
+        //-------setting autocomplete for search bar-----------
+        customerFromInput.setAdapter(autocompleteAdapter);
+        customerToInput.setAdapter(autocompleteAdapter);
 
         //------RIDE NOW ON CLICK--------
         customerRideNowButton.setOnClickListener(new View.OnClickListener() {
@@ -122,42 +170,57 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
             }
         });
 
-    }
+        searchFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customerFrom = customerFromInput.getText().toString();
+                Geocoder fromGeocoder = new Geocoder(CustomerMapActivity.this);
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+                List<Address> fromList = new ArrayList<>();
+                try {
+                    fromList = fromGeocoder.getFromLocationName(customerFrom, 1);
+                } catch (IOException e) {
 
-        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney , 10));
-        if (locationPermissionGranted) {
-            getDeviceLocation();
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
+                }
+
+                if(fromList.size() > 0){
+                    fromAddress = fromList.get(0);
+                    moveCamera(new LatLng(fromAddress.getLatitude(),fromAddress.getLongitude()), DEFAULT_ZOOM);
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(fromAddress.getLatitude(),fromAddress.getLongitude())).title("from"));
+                    Toast.makeText(CustomerMapActivity.this, "Address found", Toast.LENGTH_SHORT).show();
+                }
             }
-            mMap.setMyLocationEnabled(true);
-        }
-    }
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main_menu , menu);
-        return true;
-    }
+        searchTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customerTo = customerToInput.getText().toString();
+                Geocoder toGeocoder = new Geocoder(CustomerMapActivity.this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        super.onOptionsItemSelected(item);
-        if(item.getItemId() == R.id.main_menu_logout_button){
-            FirebaseAuth.getInstance().signOut();
-            Intent startIntent = new Intent(CustomerMapActivity.this , StartActivity.class);
-            startActivity(startIntent);
-            finish();
-        }
-        return true;
+                List<Address> toList = new ArrayList<>();
+                try {
+                    toList = toGeocoder.getFromLocationName(customerTo, 1);
+                } catch (IOException e) {
+
+                }
+
+                if(toList.size() > 0){
+                    toAddress = toList.get(0);
+                    moveCamera(new LatLng(toAddress.getLatitude(),toAddress.getLongitude()), DEFAULT_ZOOM);
+                    mMap.addMarker(new MarkerOptions().position(new LatLng(toAddress.getLatitude(),toAddress.getLongitude())).title("to"));
+                    Toast.makeText(CustomerMapActivity.this, "Address found", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        myLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDeviceLocation();
+            }
+        });
+
     }
 
     private void getLocationPermission(){
@@ -195,43 +258,11 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
         }
     }
 
-    private void mapInit(){
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    private void getDeviceLocation(){
-            if(checkLocationEnabled()){
-            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-            try{
-                if(locationPermissionGranted){
-                    Task location = fusedLocationProviderClient.getLastLocation();
-                    location.addOnCompleteListener(new OnCompleteListener() {
-                        @Override
-                        public void onComplete(@NonNull Task task) {
-                            if(task.isSuccessful()){
-                                Location currentLocation = (Location) task.getResult();
-                                moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
-                                Toast.makeText(CustomerMapActivity.this , "location found", Toast.LENGTH_SHORT).show();
-                            }else{
-                                Toast.makeText(CustomerMapActivity.this, "location not found", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }
-
-            }catch (SecurityException e){
-
-            }
-        }else{
-            getDeviceLocationPrompt();
-        }
-    }
-
-    private void moveCamera(LatLng latLng, float zoom){
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    private boolean checkLocationEnabled(){
+        LocationManager locationManager = (LocationManager)getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return gps_enabled;
     }
 
     private void getDeviceLocationPrompt(){
@@ -270,11 +301,126 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
         });
     }
 
-    private boolean checkLocationEnabled(){
-        LocationManager locationManager = (LocationManager)getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-        boolean gps_enabled = false;
-        gps_enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        return gps_enabled;
+    private void mapInit(){
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
+    private void getDeviceLocation(){
+        if(checkLocationEnabled()){
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            try{
+                if(locationPermissionGranted){
+                    Task location = fusedLocationProviderClient.getLastLocation();
+                    location.addOnCompleteListener(new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if(task.isSuccessful()){
+                                Location currentLocation = (Location) task.getResult();
+                                moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+                                Toast.makeText(CustomerMapActivity.this , "location found", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(CustomerMapActivity.this, "location not found", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+            }catch (SecurityException e){
+
+            }
+        }else{
+            getDeviceLocationPrompt();
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom){
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney , 10));
+        if (locationPermissionGranted) {
+            getDeviceLocation();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu , menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.main_menu_logout_button){
+            FirebaseAuth.getInstance().signOut();
+            Intent startIntent = new Intent(CustomerMapActivity.this , StartActivity.class);
+            startActivity(startIntent);
+            finish();
+        }
+        return true;
+    }
+
+
+    //---------Google maps places API--------------------
+    private AdapterView.OnItemClickListener suggestionClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(CustomerMapActivity.this, "Item clicked", Toast.LENGTH_SHORT).show();
+            final AutocompletePrediction items = autocompleteAdapter.getItem(position);
+            final String place_id = items.getPlaceId();
+
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi.getPlaceById(googleApiClient, place_id);
+            placeResult.setResultCallback(placeDetailsCallback);
+        }
+    };
+
+    private ResultCallback<PlaceBuffer> placeDetailsCallback;
+
+    {
+        placeDetailsCallback = new ResultCallback<PlaceBuffer>() {
+            @Override
+            public void onResult(@NonNull PlaceBuffer places) {
+                Toast.makeText(CustomerMapActivity.this, "Checking the result", Toast.LENGTH_SHORT).show();
+                if (!places.getStatus().isSuccess()) {
+                    Toast.makeText(CustomerMapActivity.this, "No places found", Toast.LENGTH_SHORT).show();
+                    places.release();
+                    return;
+                }
+                final Place place = places.get(0);
+                try {
+                    Toast.makeText(CustomerMapActivity.this, "Trying to get the location",Toast.LENGTH_SHORT).show();
+                    selectedPlace = new placeInfo();
+                    selectedPlace.setName(place.getName().toString());
+                    selectedPlace.setAddress(place.getAddress().toString());
+                    selectedPlace.setId(place.getId());
+                    selectedPlace.setAttributions(place.getAttributions().toString());
+                    selectedPlace.setLatLng(place.getLatLng());
+                    selectedPlace.setPhoneNumber(place.getPhoneNumber().toString());
+                    selectedPlace.setRating(place.getRating());
+                    selectedPlace.setWebsiteUri(place.getWebsiteUri());
+                } catch (Exception e) {
+
+                }
+                moveCamera(new LatLng(place.getViewport().getCenter().latitude, place.getViewport().getCenter().longitude), DEFAULT_ZOOM);
+                places.release();
+            }
+        };
+    }
 }
---
+++ app/src/main/java/com/niit/shreyasgs/commute/placeInfo.java
@@ -0,0 +1,97 @@
+package com.niit.shreyasgs.commute;
+
+import android.net.Uri;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class placeInfo {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String id;
+    private Uri websiteUri;
+    private LatLng latLng;
+    private float rating;
+    private String attributions;
+
+    public placeInfo(){
+
+
+    }
+
+    public placeInfo(String name, String address, String phoneNumber, String id, Uri websiteUri, LatLng latLng, float rating, String attributions) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.id = id;
+        this.websiteUri = websiteUri;
+        this.latLng = latLng;
+        this.rating = rating;
+        this.attributions = attributions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Uri getWebsiteUri() {
+        return websiteUri;
+    }
+
+    public void setWebsiteUri(Uri websiteUri) {
+        this.websiteUri = websiteUri;
+    }
+
+    public LatLng getLatLng() {
+        return latLng;
+    }
+
+    public void setLatLng(LatLng latLng) {
+        this.latLng = latLng;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getAttributions() {
+        return attributions;
+    }
+
+    public void setAttributions(String attributions) {
+        this.attributions = attributions;
+    }
+}
